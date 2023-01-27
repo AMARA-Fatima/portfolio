4b9c95b22f876a5885e271b3c5db50f577fb3d55
AMARA-Fatima <amarafati27@gmail.com>
2023-01-27 12:00:39 +0100

    initial commit

---
+++ assets/inc/footer.php
@@ -0,0 +1,3 @@
+<footer class="bg-dark text-light p-5">
+    <p> site porfolio footer</p>
+</footer>
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php 
+
+// Commencer par l'appel du controller
+// require(controller/...)
+
+define("PAGE_TITLE", "Accueil");
+
+?>
+<?php include ("./assets/inc/head.php") ?>
+<?php include ("./assets/inc/header.php") ?>
+
+<main>
+</main>
+
+<?php include ("./assets/inc/footer.php") ?>
